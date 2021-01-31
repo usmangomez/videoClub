cf67bbf84dc06da21fcb0ffef8d2e1a8fcdd7560
usmango <usmangomezabeuov@gmail.com>
2021-01-31 19:21:42 +0100

    New Features

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -1,17 +1,64 @@
+
+
 @extends('layouts.master')
 
-@section('title', 'Edit')
+@section('title', 'Create')
 
 @section('content')
 
 <div class="container">
 
-    <div class="row">
-        <div class="col-12">
-            <p>Edit</p>
-        </div>
-    </div>
+   <div class="row" style="margin-top:40px">
+      <div class="offset-md-3 col-md-6">
+         <div class="card">
+            <div class="card-header text-center">
+               Editar película
+            </div>
+            <div class="card-body" style="padding:30px">
+
+                <form action="" method="POST">
+   
+                    @csrf
+        
+                    <div class="form-group">
+                       <label for="title">Título</label>
+                       <input type="text" name="title" id="title" class="form-control" value="{{$pelicula['title']}}">
+                    </div>
+        
+                    <div class="form-group">
+                       <label for="title">Año</label>
+                       <input type="text" name="title" id="year" class="form-control" value="{{$pelicula['year']}}">
+                    </div>
+        
+                    <div class="form-group">
+                     <label for="title">Director</label>
+                     <input type="text" name="title" id="director" class="form-control" value="{{$pelicula['director']}}">
+                    </div>
+        
+                    <div class="form-group">
+                     <label for="title">URL Poster</label>
+                     <input type="text" name="title" id="image" class="form-control" value="{{$pelicula['poster']}}">
+                    </div>
+        
+                    <div class="form-group">
+                       <label for="synopsis">Resumen</label>
+                       <textarea name="synopsis" id="synopsis" class="form-control" rows="3"> value="{{$pelicula['synopsis']}}"</textarea>
+                    </div>
+        
+                    <div class="form-group text-center">
+                       <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                           Editar Pelicula
+                       </button>
+                    </div>
+                    
+                 </form>
+   
+            </div>
+         </div>
+      </div>
+   </div>
 
 </div>
     
 @endsection
+
